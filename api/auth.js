bc4ab75f73cca9be56686bdf69fdb4e486d4b1e2
isu643ismayil643-hub <allu600ashmil600@gmail.com>
2026-05-01 14:07:16 +0530

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,1 +1,21 @@
+import crypto from "crypto";
 
+export default function handler(req, res) {
+  const publicKey = process.env.IMAGEKIT_PUBLIC_KEY;
+  const privateKey = process.env.IMAGEKIT_PRIVATE_KEY;
+
+  const token = Math.random().toString(36).substring(2);
+  const expire = Math.floor(Date.now() / 1000) + 60 * 5;
+
+  const signature = crypto
+    .createHmac("sha1", privateKey)
+    .update(token + expire)
+    .digest("hex");
+
+  res.status(200).json({
+    token,
+    expire,
+    signature,
+    publicKey
+  });
+}
